1ba0129fd8177ffd1904f4c389aff0a3140f6866
Nathan Clarke <CaptainCow95@gmail.com>
2014-07-15 23:14:30 -0600

    Some small changes to the toolchain build script.

--- toolchain-build.sh
+++ toolchain-build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # install necessary packages
-sudo apt-get install make nasm gcc g++ libgmp-dev libmpfr-dev libmpc-dev genisoimage grub libgtk2.0-dev libsdl1.2-dev
+sudo apt-get install make nasm gcc g++ libgmp-dev libmpfr-dev libmpc-dev genisoimage grub libgtk2.0-dev libsdl1.2-dev bochs bochs-sdl
 
 # create folder for build
 mkdir toolchain-build
 cd toolchain-build
 
 # download all the source packages we need
-wget -O binutils-2.9.1.tar.gz ftp://ftp.gnu.org/gnu/binutils/binutils-2.24.tar.gz
-wget -O gcc-4.9.0.tar.gz ftp://ftp.gnu.org/gnu/gcc/gcc-4.9.0/gcc-4.9.0.tar.gz
-wget -O bochs-2.6.6.tar.gz http://downloads.sourceforge.net/project/bochs/bochs/2.6.6/bochs-2.6.6.tar.gz
+wget -N ftp://ftp.gnu.org/gnu/binutils/binutils-2.24.tar.gz
+wget -N ftp://ftp.gnu.org/gnu/gcc/gcc-4.9.0/gcc-4.9.0.tar.gz
+wget -N http://downloads.sourceforge.net/project/bochs/bochs/2.6.6/bochs-2.6.6.tar.gz
 
 # untar all the source packages
 tar -xf binutils-2.24.tar.gz
@@ -26,7 +26,7 @@ mkdir build-binutils
 cd build-binutils
 ../binutils-2.24/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --disable-werror
 make
-make install
+sudo make install
 
 cd ..
 
@@ -35,12 +35,11 @@ mkdir build-gcc
 cd build-gcc
 ../gcc-4.9.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
 make all-gcc
-make install-gcc
+sudo make install-gcc
 
 cd ..
 
 # and finally build bochs
-mkdir bochs-2.6.6
 cd bochs-2.6.6
 ./configure --enable-smp --enable-all-optimizations --enable-x86-64 --enable-pci --enable-vmx --enable-logging --enable-fpu --enable-3dnow --enable-sb16=dummy --enable-cdrom --disable-plugins --disable-docbook --with-sdl
 make
