3a75e254b08ac42af3c5fd10ee220a398a81d2a0
Nathan Clarke <captaincow95@gmail.com>
2014-10-28 21:33:51 -0600

    Updating the toolchain build script.

--- toolchain-build.sh
+++ toolchain-build.sh
@@ -3,8 +3,11 @@
 # install necessary packages
 sudo apt-get install make nasm gcc g++ libgmp-dev libmpfr-dev libmpc-dev genisoimage grub libgtk2.0-dev libsdl1.2-dev bochs bochs-sdl
 
+_dir="$(pwd)"
+
 # create folder for build
 mkdir toolchain-build
+# go into the build folder
 cd toolchain-build
 
 # download all the source packages we need
@@ -18,30 +21,43 @@ tar -xf gcc-4.9.0.tar.gz
 tar -xf bochs-2.6.6.tar.gz
 
 # build the cross-compiler starting with binutils
-export PREFIX="/usr/local/cross"
+export PREFIX="$_dir/compiler"
 export TARGET=i686-elf
 export PATH="$PREFIX/bin:$PATH"
 
+# build binutils
 mkdir build-binutils
+# go into binutils build folder
 cd build-binutils
 ../binutils-2.24/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --disable-werror
-make
+make -j 4
 sudo make install
 
+# out of binutils build folder
 cd ..
 
 # now gcc
 mkdir build-gcc
+# go into gcc build folder
 cd build-gcc
 ../gcc-4.9.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
-make all-gcc
+make -j 4 all-gcc
 sudo make install-gcc
 
+# out of gcc build folder
 cd ..
 
 # and finally build bochs
+# go into bochs folder
 cd bochs-2.6.6
 ./configure --enable-smp --enable-all-optimizations --enable-x86-64 --enable-pci --enable-vmx --enable-logging --enable-fpu --enable-3dnow --enable-sb16=dummy --enable-cdrom --disable-plugins --disable-docbook --with-sdl
-make
+make -j 4
 
+# out of bochs folder
 cd ..
+
+# out of build folder
+cd ..
+
+# reset ownership of the compiler output folder
+sudo chown -R $USER:$USER compiler
